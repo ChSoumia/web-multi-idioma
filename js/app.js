d8a1d7712332bad67bb10776bab83a9107e72d09
ChSoumia <ChSoumia@users.noreply.github.com>
2024-07-21 15:28:41 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const check = document.getElementById("check");
+check.addEventListener("click", idioma);
+
+function idioma() {
+  let id = check.checked;
+  if (id == true) {
+    location.href = "es/index.html";
+  } else {
+    location.href = "../index.html";
+  }
+}
